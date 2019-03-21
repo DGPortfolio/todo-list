612384f0c56bc035559677ef54a462916362b0aa
DGPortfolio <dawidgpl@gmail.com>
2019-03-21 19:03:49 +0100

    dodanie projektu do repozytorium

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  tasksList: Array<string> = [];
+  tasksDone: Array<string> = [];
+
+  add(task: string) {
+    this.tasksList.push(task);
+  }
+  remove(task: string) {
+    this.tasksList = this.tasksList.filter( e => e !== task );
+  }
+  done(task: string) {
+    this.tasksDone.push(task);
+    this.remove(task);
+  }
+}
